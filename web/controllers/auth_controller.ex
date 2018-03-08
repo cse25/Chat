bcf11daa4f58cb25941c3e6dab96c973ffa1db41
Chris Csekey <chrisjc25@gmail.com>
2018-03-07 20:57:36 -0800

    Implement AuthController

---
+++ web/controllers/auth_controller.ex
@@ -0,0 +1,44 @@
+defmodule Chat.AuthController do
+  use Chat.Web, :controller
+
+  plug Ueberauth
+
+  alias Chat.User
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, %{"provider" => provider} = params) do
+    user_params = %{personaname: auth.extra.raw_info.user.personaname, steamid: auth.extra.raw_info.user.steamid, provider: provider}
+    changeset = User.changeset(%User{}, user_params)
+    IO.puts("+++++")
+    IO.inspect(user_params)
+    IO.puts("+++++")
+    IO.inspect(conn)
+    IO.puts("+++++")
+    IO.inspect(params)
+    IO.puts("+++++")
+
+    signin(conn, changeset)
+  end
+
+  defp signin(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: chat_path(conn, :index))
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect(to: chat_path(conn, :index))
+    end
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, personaname: changeset.changes.personaname) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
+  end
+end
